cf3e923482b140a7fe0b775a24b40af5156cf66f
Arulraj A <arul.raj47@gmail.com>
2021-04-07 14:30:43 +0530

    Committed the example project.

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<GDPData> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: SfCartesianChart(
+        title: ChartTitle(text: 'Continent wise GDP - 2021'),
+        legend: Legend(isVisible: true),
+        tooltipBehavior: _tooltipBehavior,
+        series: <ChartSeries>[
+          BarSeries<GDPData, String>(
+              name: 'GDP',
+              dataSource: _chartData,
+              xValueMapper: (GDPData gdp, _) => gdp.continent,
+              yValueMapper: (GDPData gdp, _) => gdp.gdp,
+              dataLabelSettings: DataLabelSettings(isVisible: true),
+              enableTooltip: true)
+        ],
+        primaryXAxis: CategoryAxis(),
+        primaryYAxis: NumericAxis(
+            edgeLabelPlacement: EdgeLabelPlacement.shift,
+            numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0),
+            title: AxisTitle(text: 'GDP in billions of U.S. Dollars')),
+      ),
+    ));
+  }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData('Oceania', 1600),
+      GDPData('Africa', 2490),
+      GDPData('S America', 2900),
+      GDPData('Europe', 23050),
+      GDPData('N America', 24880),
+      GDPData('Asia', 34390),
+    ];
+    return chartData;
+  }
+}
+
+class GDPData {
+  GDPData(this.continent, this.gdp);
+  final String continent;
+  final double gdp;
+}
